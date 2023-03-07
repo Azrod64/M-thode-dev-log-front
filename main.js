9ea150e544f7453d4cad94861e6f2d68251fd3bf
Romuald Moisan <moisan.romuald@gmail.com>
2023-03-07 11:31:22 +0100

    Add a wbsite

---
+++ main.js
@@ -0,0 +1,17 @@
+// --------------- MODULES IMPORT ---------------
+
+
+const express = require("express");
+
+
+// --------------- SERVER SETUP ---------------
+
+
+const app = express();
+const port = 3000;  // Localhost Port
+
+app.listen(port, () => {
+    console.log(`App listening to port ${port}`);
+});
+
+app.use(express.static("public"));
