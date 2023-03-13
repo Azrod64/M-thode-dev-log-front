e31f4514045cafd2569f3ac398f22c3b7534c98c
Romuald Moisan <moisan.romuald@gmail.com>
2023-03-13 10:25:25 +0100

    Add a new form

--- main.js
+++ main.js
@@ -10,8 +10,11 @@ const express = require("express");
 const app = express();
 const port = 3000;  // Localhost Port
 
+
+
 app.listen(port, () => {
     console.log(`App listening to port ${port}`);
 });
 
 app.use(express.static("public"));
+
---
+++ public/script.js
@@ -0,0 +1,26 @@
+// var nom = document.getElementById("Nom").value;
+// console.log(nom);
+// var prenom = document.getElementById("Prenom").value;
+// var email = document.getElementById("E-mail").value;
+// var societe = document.getElementById("Societe").value;
+// var pays = document.getElementById("Pays").value;
+
+// const fileData = fs.readFileSync("/home/azrod/Documents/GitHub/M-thode-dev-log-front/public/info.json", { encoding: "utf8" });
+// const fileDataArray = fileData.split("\n");
+// const newData = `,{
+//         "id": "3",
+//         "email": "${nom}",
+//         "first": "${prenom}",
+//         "last": "${email}",
+//         "company": "${societe}",
+//         "created_at": "date",
+//         "country": "${pays}"
+//     }`;
+// const index = -1; // after each row to insert your data
+
+// fileDataArray.splice(index, 0, newData); // insert data into the array
+
+// const newFileData = fileDataArray.join("\n"); // create the new file
+
+// fs.writeFileSync("/home/azrod/Documents/GitHub/M-thode-dev-log-front/public/info.json", newFileData, { encoding: "utf8" }); // save it
+
