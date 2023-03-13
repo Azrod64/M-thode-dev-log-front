73bde6b1fe5b6493dc648a0927793641d2f5d4ed
Romuald Moisan <moisan.romuald@gmail.com>
2023-03-13 13:27:33 +0100

    Add client update

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 
 
 const express = require("express");
-
+const fs = require("fs");
 
 // --------------- SERVER SETUP ---------------
 
@@ -11,10 +11,34 @@ const app = express();
 const port = 3000;  // Localhost Port
 
 
-
 app.listen(port, () => {
     console.log(`App listening to port ${port}`);
 });
 
 app.use(express.static("public"));
+
+// --------------- RECUP POST INFORMATION ---------------
+
+
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+app.post("/index.html", function (req, res) {
+    console.log(req.body);
+    console.log(req.body.first);
+    changeJSON(req.body);
+    res.redirect("/index.html");
+});
+function changeJSON(options) {
+    const fileData = fs.readFileSync("/home/azrod/Documents/GitHub/M-thode-dev-log-front/public/info.json");
+    let JSONData = JSON.parse(fileData);
+    let newUser = Object.assign(options);
+
+    newUser.id = JSONData[JSONData.length-1].id + 1;
+    
+    newUser.created_at = new Date();
+    JSONData.push(newUser);
+
+    fs.writeFileSync("/home/azrod/Documents/GitHub/M-thode-dev-log-front/public/info.json", JSON.stringify(JSONData) ); // save it
+}
 
